9cf2263ed876f90365cde12d8752fbef5ecb8384
vijaykrishna-11 <124434331+vijaykrishna-11@users.noreply.github.com>
2023-07-17 21:23:54 +0530

    Delete leetCode directory

--- leetCode/HammingDistance.java
+++
@@ -1,6 +0,0 @@
-public class HammingDistance {
-    public int hammingDistance(int x, int y) {
-        int z = x ^ y;
-        return Integer.bitCount(z);
-    }
-}
--- leetCode/SingleNumber.java
+++
@@ -1,8 +0,0 @@
-public class SingleNumber {
-    public int singleNumber(int[] nums) {
-        int out = 0;
-        for (int i = 0; i < nums.length; i++)
-            out ^= nums[i];
-        return out;
-    }
-}
