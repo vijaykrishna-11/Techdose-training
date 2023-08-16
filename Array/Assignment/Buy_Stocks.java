8c8a7f5eb65df60f47c52a81d7056e97c1b9d88f
vijaykrishna-11 <vijaykrishna21135@gmail.com>
2023-08-16 23:33:32 +0530

    Assignment

--- Array/Assignment/Buy_Stocks.java
+++ Array/Assignment/Buy_Stocks.java
@@ -1,3 +1,21 @@
 public class Buy_Stocks {
-    
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int l = 0, r = 1;
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                int p = prices[r] - prices[l];
+                max = Math.max(p, max);
+            }
+            // System.out.println(l+" "+r+" "+max+" ");
+            else {
+                l = r;
+                // r++;
+            }
+            System.out.println(l + " " + r + " " + max + " ");
+            r++;
+
+        }
+        return max;
+    }
 }
--- Array/Assignment/Range_Sum_Query.java
+++ Array/Assignment/Range_Sum_Query.java
@@ -1,3 +1,16 @@
 public class Range_Sum_Query {
-    
+    int[] nums;
+
+    public Range_Sum_Query(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        this.nums = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        if (i == 0)
+            return nums[j];
+        return nums[j] - nums[i - 1];
+    }
 }
