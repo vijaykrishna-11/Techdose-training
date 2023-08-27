937e48c0353ef8580b79760148109d00315715fc
vijaykrishna-11 <vijaykrishna21135@gmail.com>
2023-08-27 22:30:38 +0530

    Assignment

--- Stack/ClassWork/Stack_Using_2Queue.java
+++ Stack/ClassWork/Stack_Using_2Queue.java
@@ -1,16 +1,14 @@
 import java.util.Stack;
 
 public class Stack_Using_2Queue {
-    /** Initialize your data structure here. */
-    Stack<Integer> s1;// stack 1
-    Stack<Integer> s2;// stack2
+    Stack<Integer> s1;
+    Stack<Integer> s2;
 
     public Stack_Using_2Queue() {
         s1 = new Stack<Integer>();
         s2 = new Stack<Integer>();
     }
 
-    /** Push element x to the back of queue. */
     public void push(int x) {
 
         while (!s1.isEmpty()) {
@@ -26,18 +24,15 @@ public class Stack_Using_2Queue {
 
     }
 
-    /** Removes the element from in front of queue and returns that element. */
     public int pop() {
         return s1.pop();
 
     }
 
-    /** Get the front element. */
     public int peek() {
         return s1.peek();
     }
 
-    /** Returns whether the queue is empty. */
     public boolean empty() {
         return s1.isEmpty();
     }
