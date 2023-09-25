a06614b8e045873325d8e62f2c57c9ee387cc4e5
vijaykrishna-11 <vijaykrishna21135@gmail.com>
2023-09-25 11:35:05 +0530

    Assignment

---
+++ Hash Map/Group_Anagrams.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+class Group_Anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char ch[] = strs[i].toCharArray();
+            Arrays.sort(ch);
+            String k = String.valueOf(ch);
+            if (!map.containsKey(k)) {
+                List<String> l = new ArrayList<>();
+                l.add(strs[i]);
+                map.put(k, l);
+            } else {
+                List<String> s = map.get(k);
+                s.add(strs[i]);
+                map.put(k, s);
+            }
+        }
+        System.out.println(map);
+        return new ArrayList<>(map.values());
+    }
+}
--- Hash Map/Valid_Sudoku.java
+++ Hash Map/Valid_Sudoku.java
@@ -1,4 +1,45 @@
 
 public class Valid_Sudoku {
-    
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            int hash[] = new int[10];
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    hash[board[i][j] - '0']++;
+                    if (hash[board[i][j] - '0'] > 1) {
+                        return false;
+                    }
+                }
+
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            int hash[] = new int[10];
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] != '.') {
+                    hash[board[j][i] - '0']++;
+                    if (hash[board[j][i] - '0'] > 1) {
+                        return false;
+                    }
+                }
+
+            }
+        }
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                int hash[] = new int[10];
+                for (int k = i; k < i + 3; k++) {
+                    for (int h = j; h < j + 3; h++) {
+                        if (board[k][h] != '.') {
+                            hash[board[k][h] - '0']++;
+                            if (hash[board[k][h] - '0'] > 1) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
