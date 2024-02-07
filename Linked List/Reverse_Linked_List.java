a48f45a52d8783545e6ed3e2cc026c21f6e48d39
vijaykrishna-11 <vijaykrishna21135@gmail.com>
2024-02-07 23:40:05 +0530

    Assignment

--- Linked List/Reverse_Linked_List.java
+++ Linked List/Reverse_Linked_List.java
@@ -1,4 +1,16 @@
 
 public class Reverse_Linked_List {
-    
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null)
+        {
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+        
+    }
 }
