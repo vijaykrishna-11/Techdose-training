41fee87e46498f21447d7ecf4d9e9dd103ea063d
vijaykrishna-11 <vijaykrishna21135@gmail.com>
2024-02-23 15:51:36 +0530

    Assignment

--- Dynamic Programming/Coin_Change.java
+++ Dynamic Programming/Coin_Change.java
@@ -1,5 +1,24 @@
 package Dynamic Programming;
 
 public class Coin_Change {
-    
+    public int search(int[]coins,int amount,int[] dp)
+    {
+        if(amount==0) return 0;
+        if(amount<0) return 10000000;
+        if(dp[amount]!=-1) return dp[amount];
+        int min=10000000;
+        int ans=0;
+        for(int i=0;i<coins.length;i++)
+        {
+            ans=1+search(coins,amount-coins[i], dp);
+            min=Math.min(ans,min);
+        }
+        return dp[amount]=min;
+    }
+    public int coinChange(int[] coins, int amount) {
+        int dp[]=new int[amount+1];
+        Arrays.fill(dp,-1);
+        int a=search(coins,amount,dp);
+        return a==10000000?-1:a;
+    }
 }
--- Dynamic Programming/Coin_Change_2.java
+++ Dynamic Programming/Coin_Change_2.java
@@ -1,5 +1,17 @@
 package Dynamic Programming;
 
 public class Coin_Change_2 {
-    
+    public int change(int amount, int[] coins) {
+        int dp[] = new int[amount+1];
+         int i,j;
+         int c = coins.length;
+         dp[0] = 1;
+         for(i=0;i<c;i++){
+             for(j=coins[i] ; j<=amount ; j++){
+                 dp[j] += dp[j - coins[i]];
+             }
+         }
+         
+         return dp[amount]; 
+     }
 }
